0c4be14358ee538883bcbe8807ea6f6057ee0091
Ansh Agarwal <anshagrawal12348@gmail.com>
2025-05-04 15:50:45 +0530

    feat: add getUserScore endpoint to retrieve user performance metrics

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const UserModel = require('../models/UserModel');
+const ChatModel = require('../models/ChatModel');
 const jwt = require('jsonwebtoken')
 
 
@@ -28,7 +29,45 @@ const login = async (req, res) => {
     }
 }
 
+const getUserScore = async (req, res) => {
+    const { user } = req;
+    if (!user) {
+        return res.status(401).json({ error: 'User not authenticated' });
+    }
+    const userId = user._id;
+    
+    try {
+        const chats = await ChatModel.find({ userId, isCompleted: true }).sort({ createdAt: -1 });
+        if (!chats) {
+            return res.status(404).json({ error: 'No completed chats found' });
+        }
+        const obj = {
+            vocubulary: 0,
+            content: 0,
+            confidence: 0,
+            clarity: 0,
+        }
+
+        chats.forEach(chat => {
+            obj.vocubulary += chat.score.vocubulary;
+            obj.content += chat.score.content;
+            obj.confidence += chat.score.confidence;
+            obj.clarity += chat.score.clarity;
+        });
+        obj.vocubulary = obj.vocubulary / chats.length;
+        obj.content = obj.content / chats.length;
+        obj.confidence = obj.confidence / chats.length;
+        obj.clarity = obj.clarity / chats.length;
+        res.status(200).json(obj);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+
+}
+
+
 module.exports = {
     signup,
-    login
+    login,
+    getUserScore,
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login } = require('../controllers/UserController');
+const { signup, login ,getUserScore} = require('../controllers/UserController');
+const  requireAuth  = require('../middleware/requireAuth');
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/score',requireAuth,getUserScore);
 
 module.exports = router;
